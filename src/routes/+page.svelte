526b153d674a6395f1d3b9f569fee828e51d3ca3
JustCallMeSimon <github@jcms.dev>
2024-06-14 15:20:34 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+
+<script>
+	console.log("test")
+</script>
+
+<body class="flex-center">
+	<h2 class="title text">Github pages svelte template</h2>
+	<p class="description text">Simple template for using Svelte on github pages + auto deploy</p>
+</body>
+
+<style>
+	.flex-center {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;		
+	}
+	
+	.title {
+		font-size: 50pt;
+		margin-bottom: 2vh;
+	}
+	
+	.description {
+		font-size: 20pt;
+	}
+	
+	.text {
+		color: white;
+		font-family: sans-serif;
+	}
+</style>
